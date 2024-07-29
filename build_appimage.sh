6ccb11c8bc11ad1073cd2671ce6e5a3a7f7cec4a
ErikKalkoken <erik.kalkoken@gmail.com>
2024-07-29 21:10:53 +0200

    Extract metadata for build script from FyneApp.toml

--- build_appimage.sh
+++ build_appimage.sh
@@ -3,11 +3,23 @@
 # This script builds an AppImage from a bundeled Fyne app
 # for x86_64 architectures with AppStream metadata.
 
-appname="Janice"
-packagename="janice"
-appid="io.github.erikkalkoken.janice"
+set -e
+
+# Custom variables
+buildname="janice"
+
+# Constants
 dest="temp.Appdir"
 source="temp.Source"
+
+# get tomlq
+wget https://github.com/ErikKalkoken/tomlq/releases/download/v0.1.0/tomlq-0.1.0-linux-amd64.tar.gz -O tomlq.tar.gz
+tar xf tomlq.tar.gz
+rm tomlq.tar.gz
+
+# Use variables from fyne metadata
+appname=$(./tomlq -p Details.Name FyneApp.toml)
+appid=$(./tomlq -p Details.ID FyneApp.toml)
 
 # Initialize appdir folder
 rm -rf "$source"
@@ -28,9 +40,10 @@ cp "$appid.appdata.xml" "$dest/usr/share/metainfo"
 # Create appimage
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -O linuxdeploy
 chmod +x linuxdeploy
-./linuxdeploy --appdir "$dest" -v 2 -o appimage -e "$source/usr/local/bin/$packagename"  -d "$source/usr/local/share/applications/$appid.desktop" -i "$source/usr/local/share/pixmaps/$appname.png"
+./linuxdeploy --appdir "$dest" -v 2 -o appimage -e "$source/usr/local/bin/$buildname"  -d "$source/usr/local/share/applications/$appid.desktop" -i "$source/usr/local/share/pixmaps/$appname.png"
 
 # Cleanup
 rm -rf "$source"
 rm -rf "$dest"
 rm linuxdeploy
+rm tomlq
